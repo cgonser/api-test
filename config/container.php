c151018839867d70c3222000886d4d8165212657
cgonser <carlos@gonser.com.br>
2018-08-14 01:22:56 +0200

    implement initial container definition

---
+++ config/container.php
@@ -0,0 +1,12 @@
+<?php
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+$containerBuilder = new ContainerBuilder();
+
+$loader = new YamlFileLoader($containerBuilder, new FileLocator(__DIR__));
+$loader->load('services.yaml');
+
+return $containerBuilder;
--- web/index.php
+++ web/index.php
@@ -1,3 +1,12 @@
 <?php
 
-echo " Hello World from HelloFresh";
+require_once __DIR__.'/../vendor/autoload.php';
+
+use Symfony\Component\HttpKernel\HttpKernel;
+use Symfony\Component\HttpFoundation\Request;
+
+$container = include __DIR__ . '/../config/container.php';
+
+$container->get(HttpKernel::class)
+    ->handle(Request::createFromGlobals())
+    ->send();
